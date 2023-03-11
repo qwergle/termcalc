3899eef16cc01dde837ec67bd5c5a31f4d97f3cf
supremestdoggo <83146042+supremestdoggo@users.noreply.github.com>
2023-03-10 21:41:45 -0500

    Continue adding extern "C" flags

--- src/term_gcalc.h
+++ src/term_gcalc.h
@@ -1,7 +1,13 @@
 #ifndef TERM_GCALC_H
 #define TERM_GCALC_H
+#ifdef __cplusplus
+extern "C" {
+#endif
 char *string_of_char(char s, size_t len);
 char **alloc_grid(size_t width);
 void print_grid(char **grid, size_t width);
 int term_gcalc_main();
+#ifdef __cplusplus
+}
+#endif
 #endif
